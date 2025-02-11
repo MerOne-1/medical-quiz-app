dea71e882f023ecd299917f49df119c4f2d36fa0
MerOne-1 <54721030+MerOne-1@users.noreply.github.com>
2025-02-11 19:53:39 +0100

    Restore ProjectSelect as default route

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ function App() {
                 path="/"
                 element={
                   <PrivateRoute>
-                    <HomePage />
+                    <ProjectSelect />
                   </PrivateRoute>
                 }
               />
